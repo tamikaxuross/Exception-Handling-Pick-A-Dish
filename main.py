1171aeb7a94a5e913cf152e214c2c0d5bcb74cac
tamika <tamika.ross.xu@gmail.com>
2025-04-11 13:50:40 -0500

    updated exceptions

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 def select_dish(foods, selected_food):
+    if selected_food < 0 or selected_food >= len(foods):
+        raise ValueError("Choice out of menu range.")
     print(f"Ah, {foods[selected_food]}! An excellent choice!")
 
 def your_menu(foods):
@@ -10,9 +12,13 @@ def your_menu(foods):
     
         selected_choice = int(input("Your order number? "))
         select_dish(foods, selected_choice - 1)
+    except ValueError:
+        print("Please enter a number, not letters or symbols.")
     except IndexError as error:
         print(f"{error} was entered.")
         print("Next time try entering something on the menu!")
+        print("That number is not on the menu. Try again.")
+    
 
 menu_items = [
     "Yakisoba",
